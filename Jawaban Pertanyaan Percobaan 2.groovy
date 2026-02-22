97e15dd4b3dab061ad5abf395f346e5c8965b9b8
Farrell <farrellraissa4@gmail.com>
2026-02-22 15:25:11 +0700

    Jawaban Percobaan 2

---
+++ Jawaban Pertanyaan Percobaan 2.groovy
@@ -0,0 +1,8 @@
+Jawaban Pertanyaan Percobaan 2
+1   Mahasiswa mhs1 = new Mahasiswa(); (Buat objek baru dari class Mahasiswa, simpan di memori, dan beri nama mhs1 agar bisa digunakan nanti.)
+    nama object yang dihasilkan adalah mhs1 
+2   Menggunakan operator titik  dengan format:
+    namaObjek.namaAtribut       // untuk mengakses atribut
+    namaObjek.namaMethod()      // untuk mengakses method
+3   Karena di antara pemanggilan pertama dan kedua, 
+    terdapat dua baris kode yang mengubah nilai atribut objek mhs1
