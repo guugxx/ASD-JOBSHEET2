fd0d4aea6c705f0db191847743e003b6137cfc00
Farrell <farrellraissa4@gmail.com>
2026-02-22 19:04:15 +0700

    Jawaban Pertanyaan 3

---
+++ Jawaban Pertanyaan 3.java
@@ -0,0 +1,7 @@
+Jawaban Pertanyaan 3
+1.  public Mahasiswa10(String nm, String nim, double ipk, String kls)
+2.  Membuat objek mahasiswa baru bernama mhs2 dan langsung mengisi nama, nim, ipk, dan kelas melalui constructorMembuat objek mahasiswa baru bernama mhs2 dan
+    langsung mengisi nama, nim, ipk, dan kelas melalui constructor
+3.  Method dalam class tidak harus diakses secara berurutan
+     karena setiap method bersifat independen dan dapat dipanggil sesuai kebutuhan setelah object berhasil dibuat.
+ 
